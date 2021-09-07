ac89d14132628d8d92363096ac30c3851b07ec2f
vinay sangati <vinay.sangati1993@gmail.com>
2021-09-07 17:12:49 +0530

    pallendrome

---
+++ src/com/vinay/ds/recursion/Main_Method.java
@@ -0,0 +1,25 @@
+package com.vinay.ds.recursion;
+
+public class Main_Method {
+	
+
+	
+public static void main(String[] args) {
+	int[] a = {1,2,3,4,5};
+	System.out.println(RecursionProblems.arrayProd(a,4));
+	System.out.println(RecursionProblems.factorial(10));
+	System.out.println(RecursionProblems.fibonacci(10));
+	System.out.println(RecursionProblems.adddigits(12345));
+	System.out.println(RecursionProblems.power(4,3));
+	System.out.println(RecursionProblems.binaryOut(1000));
+	System.out.println(RecursionProblems.reverse("LET"));
+	System.out.println(RecursionProblems.isPallendrome("TJET"));
+	
+}
+}
+
+
+
+
+
+
--- src/com/vinay/ds/recursion/RecursionProblems.java
+++ src/com/vinay/ds/recursion/RecursionProblems.java
@@ -13,6 +13,28 @@ public static String binaryOut(int n) {
 }
 
 public static int arrayProd(int[] arr,int remaining) {
-	return remaining == 1 ? arr[remaining] : arr[remaining] * arrayProd(arr,remaining-1);
+	return remaining == 1 ? arr[remaining] :
+		arr[remaining] * arrayProd(arr,remaining-1);
+}
+
+public static String reverse(String val) {
+	return val.length() == 1 ? val : reverse(val.substring(1)) + val.substring(0,1);
+	
+}
+public static int factorial(int val) {
+	return (val == 0 || val == 1) ? 1:val * factorial(val - 1);
+}
+
+public static int fibonacci(int n) {
+	return (n == 0 || n== 1) ?  n :  fibonacci(n-1)+fibonacci(n-2);
+}
+public static boolean isPallendrome(String val) {
+	
+	return val.length() == 1 ? true :
+		val.length() == 2 ? val.charAt(0) == val.charAt(1)  ? true : false:
+		val.charAt(0) == val.charAt(val.length()-1) ?
+	isPallendrome(val.substring(1, val.length()-1)) : 
+		false;
+	
 }
 }
